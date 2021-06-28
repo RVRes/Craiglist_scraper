a11ff889c6f80911bbc3f28569d0fe78166390f0
scorpion <rv.reshetnikov@gmail.com>
2021-06-27 20:11:52 -0500

    telegram_bot_v0.9 test autodeploy_2

--- autodeploy.sh
+++ autodeploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Переходим в категорию GIT
+PATH=$PATH:'/bin':'/bin/git':'/home/roman/scripts/craiglis_scrap'
 cd '/home/roman/scripts/craiglis_scrap'
+
 
 # Загружаем данные из ветки main
 git checkout main
